8f6703c98a2ebc8b63f14b4c9347b20e1e3af9c8
Mike Baldini <mbaldini@live.com>
2017-08-15 11:40:12 -0700

    commit

--- history.js
+++ history.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 function doWait() {
     var rnd = Math.random();
-    // set a timeout for somewhere between 180 and 60 minutes
-    var timeout = Math.round(((120 * rnd) + 60) * 60);
+    // set a timeout for somewhere between 120 and 10 minutes
+    var timeout = Math.round(((110 * rnd) + 10) * 60);
     var dt = new Date(new Date().getTime() + timeout * 1000);
     log('next update in ' + (Math.round((timeout / 60) * 100) / 100) + ' minutes at ' + dt.toTimeString());
     // log('waiting ' + timeout + ' seconds (' + (Math.round((timeout / 60) * 100) / 100) + ' minutes)');
